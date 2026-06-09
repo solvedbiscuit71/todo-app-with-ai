548df4a034301e1c864db6783f334742851e9d42
Praveen Perumal <ppraveen98841@gmail.com>
2026-06-09 07:08:58 +0530

    Create todo dto

---
+++ services/dto/todo.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class CreateTodoRequest(BaseModel):
+    title: str
+    description: str | None = None
+
+class CreateTodoResponse(BaseModel):
+    id: int
+    message: str
+
+class GetTodoResponse(BaseModel):
+    id: int
+    title: str
+    description: str | None = None
+    is_completed: bool
