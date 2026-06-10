5b034ffad4d2019f04dfc77bb3fa14a62a40ce70
Praveen Perumal <ppraveen98841@gmail.com>
2026-06-10 07:14:14 +0530

    Integrate with peewee ORM and Sqlite database

--- services/app.py
+++ services/app.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from dto.todo import CreateTodoRequest, CreateTodoResponse, GetTodoResponse
-from model.todo import TodoItem, TodoList
-
-todo_list = TodoList()
+from model.todo import TodoItem 
+from peewee import DoesNotExist
 
 app = FastAPI(root_path="/api/v1")
 
 @app.post("/todo",  status_code=201)
 async def create_todo(body: CreateTodoRequest) -> CreateTodoResponse:
-    todo_item = TodoItem(body.title, body.description)
-    todo_list.add(todo_item)
+    todo_item = TodoItem.create(title=body.title, description=body.description)
 
     return CreateTodoResponse(id=todo_item.id,
                               message="todo item created")
@@ -21,15 +19,16 @@ async def get_all_todos() -> list[GetTodoResponse]:
                         title=todo_item.title,
                         description=todo_item.description,
                         is_completed=todo_item.is_completed)
-        for todo_item in todo_list
+        for todo_item in TodoItem.select()
     ]
 
 @app.get("/todo/{id}")
 async def get_todo(id: int) -> GetTodoResponse:
-    for todo_item in todo_list:
-        if todo_item.id == id:
-            return GetTodoResponse(id=todo_item.id,
-                                   title=todo_item.title,
-                                   description=todo_item.description,
-                                   is_completed=todo_item.is_completed)
-    raise HTTPException(status_code=404, detail=f"todo item #{id} is not found")
+    try:
+        todo_item = TodoItem.get_by_id(id)
+        return GetTodoResponse(id=todo_item.id,
+                                title=todo_item.title,
+                                description=todo_item.description,
+                                is_completed=todo_item.is_completed)
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail=f"todo item #{id} is not found")
--- services/model/todo.py
+++ services/model/todo.py
@@ -1,41 +1,15 @@
-from typing import ClassVar
+from peewee import BooleanField, CharField, SqliteDatabase, Model
 
-class IntSequence:
-    value: int
+db = SqliteDatabase("todo.db")
 
-    def __init__(self, initial_value: int) -> None:
-        self.value = initial_value
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-    def __next__(self):
-        next_value = self.value
-        self.value += 1
-        return next_value
+class TodoItem(BaseModel):
+    title = CharField()
+    description = CharField(null=True)
+    is_completed = BooleanField(default=False)
 
-class TodoItem:
-    _id_seq: ClassVar[IntSequence] = IntSequence(1001)
-
-    id: int
-    title: str
-    description: str | None
-    is_completed: bool
-
-    def __init__(self, title: str, description: str | None = None) -> None:
-        self.id = next(TodoItem._id_seq)
-        self.title = title
-        self.description = description
-        self.is_completed = False
-    
-    def __repr__(self) -> str:
-        return f"TodoItem(id={self.id}, title='{self.title}', is_completed={self.is_completed})"
-
-class TodoList:
-    todos: list[TodoItem]
-
-    def __init__(self) -> None:
-        self.todos = []
-
-    def add(self, todo: TodoItem):
-        self.todos.append(todo)
-
-    def __iter__(self):
-        return iter(self.todos)
+db.connect()
+db.create_tables([TodoItem])
