debb82ae62b4ef583479e1f51ab995463b1d5aa3
Praveen Perumal <ppraveen98841@gmail.com>
2026-06-12 07:48:38 +0530

    Added route for rendering the todo list

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TodoList } from './todo-list/todo-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "todos"
+    },
+    {
+        path: "todos",
+        component: TodoList
+    },
+];
