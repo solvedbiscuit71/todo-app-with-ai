4376722ac27cccceb459d0990590dc7cbe2928c1
Praveen Perumal <ppraveen98841@gmail.com>
2026-06-14 08:21:50 +0530

    Moved todo-item and todo-list component

--- app/src/app/components/todo-list/todo-list.ts
+++ app/src/app/components/todo-list/todo-list.ts
@@ -1,18 +1,28 @@
 import { Component, inject, signal } from '@angular/core';
-import { TodoService } from '../services/todo';
+import { TodoService } from '../../services/todo';
 import { TodoItem, TodoItemInfo } from '../todo-item/todo-item';
+import { NewTodoForm } from "../new-todo-form/new-todo-form";
 
 @Component({
   selector: 'app-todo-list',
-  imports: [TodoItem],
+  imports: [TodoItem, NewTodoForm],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
 export class TodoList {
+  showNewTodo = signal<boolean>(false)
   todoList = signal<TodoItemInfo[]>([])
   todoService = inject(TodoService)
 
   constructor() {
     this.todoService.fetchAllTodos().then(this.todoList.set)
+  }
+
+  showForm() {
+    this.showNewTodo.set(true)
+  }
+
+  hideForm() {
+    this.showNewTodo.set(false)
   }
 }
